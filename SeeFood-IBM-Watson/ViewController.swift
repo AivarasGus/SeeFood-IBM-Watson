0d55bde0311daa83b78c9aea73f4ef12a729137f
Aivaras <aivarasgust@gmail.com>
2019-06-21 16:35:14 +0300

    Initial Commit

---
+++ SeeFood-IBM-Watson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood-IBM-Watson
+//
+//  Created by Aivaras Gustys on 21/06/2019.
+//  Copyright © 2019 Aivaras Gustys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
